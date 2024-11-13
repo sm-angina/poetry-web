a6362b5c5ac8fe0cd2a3862ce87471c48c2113c2
Sm Angina <bdbakka33@gmail.com>
2024-11-13 09:45:13 +0600

    modified files

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const stars1 = document.getElementById('stars1').querySelectorAll('.fa-star')
+        const stars2 = document.getElementById('stars2').querySelectorAll('.fa-star')
+        const color = ['#4CC9FE','#51C4D3', '#B0EBB4', '#09C6AB', '#B8FFD0','#4CC9FE','#D2E0FB','#B6FFFA', '#863A6F']
+
+        const animation = ['animation-timing-function: cubic-bezier(.68,.25,.83,.67);', 'animation-timing-function: cubic-bezier(.68,.25,.41,.86);', 'animation-timing-function: cubic-bezier(.25,.62,.99,.56);']
+        
+        setInterval('fallStar(stars1)', 2500)
+        setInterval('fallStar(stars2)', 2890)
+
+
+        
+        function fallStar(stars) {
+            let i = Math.floor(Math.random()* stars.length)
+            let c = Math.floor(Math.random()* color.length)
+            let a = Math.floor(Math.random()* animation.length)
+            stars[i].style = `color: ${color[c]}; animation-name: stars; ${animation[a]}`
+            setTimeout(() => {
+            stars[i].style = `animation: none`
+                
+            }, 2490);
+        }
+
+        // const cursor = document.querySelector('#cursor')
+        // document.addEventListener('mousemove', (e) => {
+
+        //     cursor.style.left = `${e.screenX}px`
+        //     cursor.style.top = `${e.screenY}px`
+            
+        //     // window.addEventListener('mouseenter',() => {console.log('leaved')})
+            
+        // })
+
