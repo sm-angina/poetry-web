628c47ce13b48d6c866ebf65bccfd30dd4443861
Sm Angina <bdbakka33@gmail.com>
2024-11-15 18:30:24 +0600

    bg-fixed + added 1

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
-const stars1 = document.getElementById('stars1').querySelectorAll('.fa-star')
-        const stars2 = document.getElementById('stars2').querySelectorAll('.fa-star')
-        const color = ['#4CC9FE','#51C4D3', '#B0EBB4', '#09C6AB', '#B8FFD0','#4CC9FE','#D2E0FB','#B6FFFA', '#863A6F']
+const stars1 = document.getElementById("stars1").querySelectorAll(".fa-star");
+const stars2 = document.getElementById("stars2").querySelectorAll(".fa-star");
+const color = [
+  "#4CC9FE",
+  "#51C4D3",
+  "#B0EBB4",
+  "#09C6AB",
+  "#B8FFD0",
+  "#4CC9FE",
+  "#D2E0FB",
+  "#B6FFFA",
+  "#863A6F",
+];
 
-        const animation = ['animation-timing-function: cubic-bezier(.68,.25,.83,.67);', 'animation-timing-function: cubic-bezier(.68,.25,.41,.86);', 'animation-timing-function: cubic-bezier(.25,.62,.99,.56);']
-        
-        setInterval('fallStar(stars1)', 2500)
-        setInterval('fallStar(stars2)', 2890)
+const animation = [
+  "animation-timing-function: cubic-bezier(.68,.25,.83,.67);",
+  "animation-timing-function: cubic-bezier(.68,.25,.41,.86);",
+  "animation-timing-function: cubic-bezier(0,.74,1,.39)",
+  "animation-timing-function: cubic-bezier(0,1.42,1,.39);",
+  "animation-timing-function: cubic-bezier(.25,.62,.99,.56);",
+];
 
+setInterval("fallStar(stars1)", 2500);
+setInterval("fallStar(stars2)", 2890);
 
-        
-        function fallStar(stars) {
-            let i = Math.floor(Math.random()* stars.length)
-            let c = Math.floor(Math.random()* color.length)
-            let a = Math.floor(Math.random()* animation.length)
-            stars[i].style = `color: ${color[c]}; animation-name: stars; ${animation[a]}`
-            setTimeout(() => {
-            stars[i].style = `animation: none`
-                
-            }, 2490);
-        }
+function fallStar(stars) {
+  let i = Math.floor(Math.random() * stars.length);
+  let c = Math.floor(Math.random() * color.length);
+  let a = Math.floor(Math.random() * animation.length);
+  stars[i].style = `color: ${color[c]}; animation-name: stars; ${animation[a]}`;
+  setTimeout(() => {
+    stars[i].style = `animation: none`;
+  }, 2490);
+}
 
-        let poetBox = document.querySelectorAll('.poet-box')
-        poetBox.forEach(element => element.addEventListener('click', ()=> element.classList.toggle('box-shadow')) );
+let poemBox = document.querySelectorAll(".poem-box");
+poemBox.forEach((element) =>
+  element.addEventListener("click", () =>
+    element.classList.toggle("box-shadow")
+  )
+);
